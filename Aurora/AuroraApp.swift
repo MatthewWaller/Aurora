b4b4dd791f1f8cbffc4f3710f7964652c51597ab
Matthew Waller <matthew@blueshiftdevelopment.com>
2021-03-11 19:25:06 -0600

    Initial Commit

---
+++ Aurora/AuroraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraApp.swift
+//  Aurora
+//
+//  Created by Matthew Waller on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct AuroraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
